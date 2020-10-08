b52af0068a2508e4c6b7945a096e6cb453fda391
Rory Jarrard <roryjarrard@gmail.com>
2020-10-08 16:56:40 -0600

    fix cors with heroku proxy

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // Get quote from API
 async function getQuote() {
-  const apiUrl = 'http://api.forsmatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
     console.log(data);
   } catch(error) {
+    getQuote();
     console.error('whoops, no quote', error);
   }
 }
