f6eeb4655a405525f2e7a19f8ea943df11ac51f4
Rory Jarrard <roryjarrard@gmail.com>
2020-10-12 16:16:21 -0600

    improve naming for loading spinner functions

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
 // Show loading
-function loading() {
+function showLoadingSpinner() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
 // Hide loading
-function complete() {
+function removeLoadingSpinner() {
   if (!loader.hidden) {
     quoteContainer.hidden = false;
     loader.hidden = true;
@@ -21,7 +21,7 @@ function complete() {
 
 // Get quote from API
 async function getQuote() {
-  loading();
+  showLoadingSpinner();
 
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
@@ -45,7 +45,7 @@ async function getQuote() {
     quoteText.innerText = data.quoteText;
 
     // Stop loader, show quote
-    complete();
+    removeLoadingSpinner();
   } catch(error) {
     getQuote().then();
     console.error('whoops, no quote', error);
